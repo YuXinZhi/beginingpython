5017f6e80d362a154854af8767cb4f8dc66bb4eb
Ben <whoyumengmeng@gmail.com>
2017-07-31 01:11:55 +0800

    chapter14net

---
+++ review/generartor.py
@@ -0,0 +1,14 @@
+'''
+生成器 带yield语句
+'''
+nested = [[1,2],[3,4],[5]]
+
+def flatten(nested):
+    for sublist in nested:
+        for element in sublist:
+            yield element
+
+
+for num in flatten(nested):
+    print(num,end='')
+
